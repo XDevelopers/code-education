aa6e24f0b0088be93509e54dda8f20a1ba1e06b1
Marcio Lourenco <marcio.l@lendfoundry.com>
2017-07-17 10:42:39 -0300

    - lesson- 002 - created a component; - it is working fine;

---
+++ lesson-002/src/app/task-list/task-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-task-list',
+    templateUrl: './task-list.component.html',
+    styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+ 
+    tasks = [];
+    task = "";
+
+    add() {
+        this.tasks.push(this.task);
+        this.task = "";
+    }
+}
